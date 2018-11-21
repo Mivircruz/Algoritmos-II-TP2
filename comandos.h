ceb7553f06fd6b8dcb554fe1e2c61694743dc004
Milagros Cruz <mivircruz@gmail.com>
2018-11-21 00:28:37 -0300

    actualizacion

--- comandos.h
+++ comandos.h
@@ -3,31 +3,24 @@
 
 #define CMD_AGREGAR_ARCHIVO			"agregar_archivo"
 #define CMD_POS_AGREGAR_ARCHIVO		1
-#define CANT_CMD_AGREGAR_ARCH		2
 
 #define CMD_VER_TABLERO				"ver_tablero"
 #define CMD_POS_CANT_VUELOS			1
 #define CMD_POS_MODO				2
 #define CMD_FECHA_VT_INICIO			3
 #define CMD_FECHA_VT_FIN			4
-#define CANT_CMD_VER_TABLERO		5
 #define MODO_ASCENDENTE				"asc"
 #define MODO_DESCENDENTE			"desc"
 
 #define CMD_INFORMACION_VUELO		"info_vuelo"
 #define CMD_POS_CODIGO_VUELO		1
-#define CANT_CMD_INFO_VUELO			2
 
 #define CMD_PRIORIDAD_VUELOS		"prioridad_vuelos"
 #define CMD_POS_PRIO_VUELO			1
-#define CANT_CMD_PRIO_VUELO			2
 
 #define CMD_BORRAR_VUELOS			"borrar"
 #define CDM_FECHA_BORRAR_INICIO		1
 #define CDM_FECHA_BORRAR_FIN		2
-#define CANT_CMD_BORRAR_VUELOS		3
-
-
 
 typedef enum{
 	AGREGAR_ARCHIVO,
@@ -36,5 +29,7 @@ typedef enum{
 	PRIORIDAD_VUELOS,
 	BORRAR_VUELO
 }comando_t;
+
+bool validar_argumentos(char* linea[], comando_t* comando, long int* vuelos);
 
 #endif
