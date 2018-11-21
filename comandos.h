44b26eaccd2f7a9100c0a066be69c04f84dd7624
Milagros Cruz <mivircruz@gmail.com>
2018-11-21 00:01:05 -0300

    comandos

--- comandos.h
+++ comandos.h
@@ -8,18 +8,23 @@
 #define CMD_VER_TABLERO				"ver_tablero"
 #define CMD_POS_CANT_VUELOS			1
 #define CMD_POS_MODO				2
+#define CMD_FECHA_VT_INICIO			3
+#define CMD_FECHA_VT_FIN			4
+#define CANT_CMD_VER_TABLERO		5
 #define MODO_ASCENDENTE				"asc"
 #define MODO_DESCENDENTE			"desc"
-#define CANT_CMD_VER_TABLERO		5
 
 #define CMD_INFORMACION_VUELO		"info_vuelo"
 #define CMD_POS_CODIGO_VUELO		1
 #define CANT_CMD_INFO_VUELO			2
 
 #define CMD_PRIORIDAD_VUELOS		"prioridad_vuelos"
+#define CMD_POS_PRIO_VUELO			1
 #define CANT_CMD_PRIO_VUELO			2
 
 #define CMD_BORRAR_VUELOS			"borrar"
+#define CDM_FECHA_BORRAR_INICIO		1
+#define CDM_FECHA_BORRAR_FIN		2
 #define CANT_CMD_BORRAR_VUELOS		3
 
 
