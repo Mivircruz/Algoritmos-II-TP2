92fcbcf4fc6df59b3c8aed6606ce8870e5fbe636
Milagros Cruz <mivircruz@gmail.com>
2018-11-21 01:00:26 -0300

    actualizacion

--- comandos.c
+++ comandos.c
@@ -5,6 +5,9 @@
 #include "comandos.h"
 #include "strutil.h"
 
+//Compara las fechas de inicio y fin. 
+//Devuelve true en caso de que la fecha de inicio sea menor que la de fin, false en caso contrario.
+
 bool comparar_fechas(char* fecha_inicio, char* fecha_fin){
 
 	char** anio_mes_inicio = split(fecha_inicio,'-');
@@ -25,36 +28,54 @@ bool comparar_fechas(char* fecha_inicio, char* fecha_fin){
 	return true;
 }
 
-bool validar_argumentos(char* argv[], comando_t* comando, long int* vuelos){	
+//Elimina el salto de línea para procesar las líneas
+
+void quitar_salto_linea(char* linea){
+
+	size_t len = strlen(linea);
+	if(linea[len-1] == '\n')
+		linea[len-1] = '\0';
+
+}
+
+//Valida los comandos pasados por parámetro
+
+bool validar_argumentos(char* linea[], comando_t* comando, long int* vuelos){	
+	
 	char* ptr;
 
-	if(!strcmp(*argv, CMD_AGREGAR_ARCHIVO))
+	for(size_t i = 0; linea[i]; i++)
+		quitar_salto_linea(linea[i]);
+
+	if(!strcmp(*linea, CMD_AGREGAR_ARCHIVO))
 		*comando = AGREGAR_ARCHIVO;
 
-	if(!strcmp(*argv, CMD_BORRAR_VUELOS)){
+	if(!strcmp(*linea, CMD_BORRAR_VUELOS)){
 
-		if(!comparar_fechas(argv[CDM_FECHA_BORRAR_INICIO], argv[CDM_FECHA_BORRAR_FIN]))
+		if(!comparar_fechas(linea[CDM_FECHA_BORRAR_INICIO], linea[CDM_FECHA_BORRAR_FIN]))
 			return false;
 		*comando = BORRAR_VUELO;
 	}
 	else{
 
-		if(!strcmp(*argv, CMD_VER_TABLERO)){
-			*vuelos = strtol(argv[CMD_POS_CANT_VUELOS],&ptr, 10);
-			if(strcmp(argv[CMD_POS_MODO], MODO_ASCENDENTE) && strcmp(argv[CMD_POS_MODO],MODO_DESCENDENTE))
+		if(!strcmp(*linea, CMD_VER_TABLERO)){
+
+			*vuelos = strtol(linea[CMD_POS_CANT_VUELOS],&ptr, 10);
+			if(strcmp(linea[CMD_POS_MODO], MODO_ASCENDENTE) && strcmp(linea[CMD_POS_MODO],MODO_DESCENDENTE))
 				return false;
-			if(!comparar_fechas(argv[CMD_FECHA_VT_INICIO], argv[CMD_FECHA_VT_FIN]))
+			if(!comparar_fechas(linea[CMD_FECHA_VT_INICIO], linea[CMD_FECHA_VT_FIN]))
 				return false;
 			*comando = VER_TABLERO;
 		}
 
-		if(!strcmp(*argv, CMD_INFORMACION_VUELO)){
-			*vuelos = strtol(argv[CMD_POS_CODIGO_VUELO],&ptr, 10);
+		if(!strcmp(*linea, CMD_INFORMACION_VUELO)){
+			*vuelos = strtol(linea[CMD_POS_CODIGO_VUELO],&ptr, 10);
 			*comando = INFORMACION_VUELO;
 		}
 
-		if(!strcmp(*argv, CMD_PRIORIDAD_VUELOS)){
-			*vuelos = strtol(argv[CMD_POS_PRIO_VUELO],&ptr, 10);
+		if(!strcmp(*linea, CMD_PRIORIDAD_VUELOS)){
+
+			*vuelos = strtol(linea[CMD_POS_PRIO_VUELO], &ptr, 10);
 			*comando = PRIORIDAD_VUELOS;
 		}
 
