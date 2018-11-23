5d1f8bb489c40a316030d49fdf9ed37c6e74294a
Milagros Cruz <mivircruz@gmail.com>
2018-11-23 11:49:46 -0300

    main

--- main.c
+++ main.c
@@ -3,26 +3,40 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "comandos.h"
 #include "strutil.h"
+#include "hash.h"
+#include "abb.h"
 
 int main(int argc, char* argv[]){
 
 //Inicialización de variables 
 
-	comando_t comando = SIN_COMANDO;
+	char* comando;
+	comando = NULL;
 	long int vuelo = 0;
 	char* linea = NULL;
 	size_t capacidad = 0;
+	hash_t* hash = hash_crear(NULL);
+	abb_t* abb = abb_crear(strcmp, NULL);
 
 //Procesamiento de comandos
 
 	while(getline(&linea, &capacidad, stdin) != -1){
-	if(!validar_argumentos(split(linea, ' '), &comando, &vuelo)){
-		printf("ERROR :)\n");
-	}
-	else
-		printf("NO ERROR :)\n");
+
+		if(!validar_argumentos(split(linea, ' '), &comando, &vuelo)){
+			hash_destruir(hash);
+			abb_destruir(abb);
+			if(!comando)
+				fprintf(stderr, "Error\n");
+			else{
+				free(comando);
+				fprintf(stderr, "Error en comando %s\n", comando);
+			}
+		}
+		else
+			printf("NO ERROR :)\n");
 	}
 	return 0;
 }
