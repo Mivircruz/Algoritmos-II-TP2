27a3bf67c63affeb8ddb8b759d565627abbab2d1
Milagros Cruz <mivircruz@gmail.com>
2018-11-23 14:00:29 -0300

    errores

--- procesamiento.h
+++ procesamiento.h
@@ -4,12 +4,15 @@
 #define POS_NUMERO_VUELO		0
 #define POS_FECHA_VUELO			6
 
-//Procesa el archivo con fornato csv y guarda la información de los vuelos.
-void agregar_archivo(FILE* archivo,hash_t* hash, abb_t* abb);
+//Procesa el archivo con formato csv y guarda la información de los vuelos.
+bool agregar_archivo(FILE* archivo,hash_t* hash, abb_t* abb);
 
 
 //En caso de encontrar el código de vuelo, devuelve true.
 //En caso contrario, devuelve false.
 bool ver_informacion_vuelo(hash_t* hash, char* codigo_vuelo, char** informacion);
+
+//Imprime una cantidad de vuelos pasada por parámetro según su prioridad.
+bool prioridad_vuelos(hash_t* hash, size_t cantidad_vuelos);
 
 #endif
