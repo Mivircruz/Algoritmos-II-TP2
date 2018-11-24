5514e11e933769a5651ef5f99aeef4687631d57b
nicodebole <43222401+nicodebole@users.noreply.github.com>
2018-11-24 15:27:32 -0300

    Update procesamiento.h

--- procesamiento.h
+++ procesamiento.h
@@ -5,12 +5,12 @@
 #define POS_FECHA_VUELO			6
 
 //Procesa el archivo con formato csv y guarda la información de los vuelos.
-bool agregar_archivo(FILE* archivo,hash_t* hash, abb_t* abb);
+bool agregar_archivo(char* nombre_archivo, hash_t* hash, abb_t* abb);
 
 
 //En caso de encontrar el código de vuelo, devuelve true.
 //En caso contrario, devuelve false.
-bool ver_informacion_vuelo(hash_t* hash, char* codigo_vuelo, char** informacion);
+bool ver_informacion_vuelo(hash_t* hash, long int* codigo_vuelo, char* informacion);
 
 //Imprime una cantidad de vuelos pasada por parámetro según su prioridad.
 bool prioridad_vuelos(hash_t* hash, size_t cantidad_vuelos);
