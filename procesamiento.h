6e60fa5e8d9141a1ee84e4b7b27403392e8dc492
Milagros Cruz <mivircruz@gmail.com>
2018-11-26 11:53:38 -0300

    procesamiento ok

--- procesamiento.h
+++ procesamiento.h
@@ -10,7 +10,7 @@ bool agregar_archivo(char* nombre_archivo, hash_t* hash, abb_t* abb);
 
 //En caso de encontrar el código de vuelo, devuelve true.
 //En caso contrario, devuelve false.
-bool ver_informacion_vuelo(hash_t* hash, long int* codigo_vuelo, char* informacion);
+bool ver_informacion_vuelo(hash_t* hash, char* codigo_vuelo);
 
 //Imprime una cantidad de vuelos pasada por parámetro según su prioridad.
 bool prioridad_vuelos(hash_t* hash, size_t cantidad_vuelos);
