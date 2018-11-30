10d84a063d18ed0eac68f5ab9e6b57421d78846d
nicodebole <43222401+nicodebole@users.noreply.github.com>
2018-11-30 19:17:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ int main(int argc, char* argv[]){
 	char* comando;
 	char* linea = NULL;
 	size_t capacidad = 0;
-	hash_t* hash = hash_crear(free);
-	abb_t* abb = abb_crear(strcmp, free);
+	hash_t* hash = hash_crear(NULL);
+	abb_t* abb = abb_crear(strcmp, NULL);
 	char** vector_linea;
 
 //Procesamiento de comandos
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]){
 		
 		comando = NULL;
 		vector_linea = split(linea, ' ');
-		if(!ejecutar_comandos(vector_linea, hash, abb, &comando)){
+		if(!ejecutar_comandos(vector_linea, linea, hash, abb, &comando)){
 			if(!comando)
 				fprintf(stderr, "Error\n");
 			else
@@ -45,4 +45,6 @@ int main(int argc, char* argv[]){
 	abb_destruir(abb);
 	return 0;
 }
+
+
 
