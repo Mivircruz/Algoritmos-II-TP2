6b3566d5c8c6de86269291f53a1e7779cbd86adf
Milagros Cruz <mivircruz@gmail.com>
2018-12-03 13:49:54 -0300

    cambios con el nuevo archivo

--- comandos.c
+++ comandos.c
@@ -9,58 +9,13 @@
 #include "hash.h"
 #include "abb.h"
 #include "strutil.h"
+#include "vuelos.h"
 
+/* ******************************************************************
+ *                        FUNCIONES AUXILIARES
+ * *****************************************************************/
 
-
-void liberar_vectores(char** v1, char** v2, char** v3, char** v4){
-	free_strv(v1);
-	free_strv(v2);
-	free_strv(v3);
-	free_strv(v4);
-}
-
-bool comparar_fechas(char* fecha_inicio, char* fecha_fin){
-
-	char** anio_mes_inicio = split(fecha_inicio,'-');
-	char** anio_mes_fin = split(fecha_fin, '-');
-	char** dia_inicio = split(anio_mes_inicio[2], 'T');
-	char** dia_fin = split(anio_mes_fin[2], 'T');
-	
-	char* ptr;
-	
-	if(strtol(anio_mes_fin[0],&ptr, 10) < strtol(anio_mes_inicio[0],&ptr, 10)){
-		liberar_vectores(anio_mes_inicio, anio_mes_fin, dia_inicio, dia_fin);
-		return false;
-	}
-	if(strtol(anio_mes_fin[0],&ptr, 10) == strtol(anio_mes_inicio[0],&ptr, 10)){
-		if(strtol(anio_mes_fin[1],&ptr, 10) < strtol(anio_mes_inicio[1],&ptr, 10)){
-			liberar_vectores(anio_mes_inicio, anio_mes_fin, dia_inicio, dia_fin);
-			return false;
-		}
-			
-		if(strtol(anio_mes_fin[1],&ptr, 10) == strtol(anio_mes_inicio[1],&ptr, 10)){	
-		
-			if(strtol(*dia_fin,&ptr, 10) < strtol(*dia_inicio,&ptr, 10)){
-				liberar_vectores(anio_mes_inicio, anio_mes_fin, dia_inicio, dia_fin);
-				return false;
-			}
-		}
-	}
-	liberar_vectores(anio_mes_inicio, anio_mes_fin, dia_inicio, dia_fin);
-
-	return true;
-}
-
-bool vuelo_valido(char* linea, long int* vuelos){
-
-	char* ptr;
-	*vuelos = strtol(linea,&ptr, 10);
-
-	if(*vuelos <= 0 || *ptr)
-		return false;
-	return true;
-}
-
+//Devuelve la cantidad de parámetros que contiene la línea
 int cantidad_parametros(char* linea){
 	
 	char** vector = split(linea, ' ');
@@ -75,8 +30,7 @@ int cantidad_parametros(char* linea){
 	return contador;
 }
 
-//Elimina el salto de línea para procesar las líneas
-
+//Elimina el salto de línea 
 void quitar_salto_linea(char* linea){
 
 	size_t len = strlen(linea);
@@ -85,7 +39,12 @@ void quitar_salto_linea(char* linea){
 
 }
 
+/* ******************************************************************
+ *                        FUNCIONES PRINCIPALES
+ * *****************************************************************/
+
 void quitar_salto_en_arreglo(char** arreglo){
+
 	for(size_t i = 0; arreglo[i]; i++)
 		quitar_salto_linea(arreglo[i]);
 }
@@ -119,7 +78,7 @@ bool ejecutar_comandos(char* linea[], char* linea_, hash_t* hash, abb_t* abb, ch
 
 	if(!strcmp(*linea, CMD_VER_TABLERO)){
 		*comando = strdup(CMD_VER_TABLERO);
-		if(!vuelo_valido(linea[CMD_POS_CANT_VUELOS], &vuelos) || !comparar_fechas(linea[CMD_FECHA_VT_INICIO], linea[CMD_FECHA_VT_FIN]))
+		if(!cantidad_vuelos_valida(linea[CMD_POS_CANT_VUELOS], &vuelos) || !comparar_fechas(linea[CMD_FECHA_VT_INICIO], linea[CMD_FECHA_VT_FIN]))
 			return false;
 		if(strcmp(linea[CMD_POS_MODO], MODO_ASCENDENTE) && strcmp(linea[CMD_POS_MODO],MODO_DESCENDENTE))
 			return false;
@@ -138,7 +97,7 @@ bool ejecutar_comandos(char* linea[], char* linea_, hash_t* hash, abb_t* abb, ch
 	}
 	if(!strcmp(*linea, CMD_PRIORIDAD_VUELOS)){
 		*comando = strdup(CMD_PRIORIDAD_VUELOS);
-		if(!vuelo_valido(linea[CMD_POS_PRIO_VUELO], &vuelos))
+		if(!cantidad_vuelos_valida(linea[CMD_POS_PRIO_VUELO], &vuelos))
 			return false;
 		if(cantidad_parametros(linea_) != 2)
 			return false;
